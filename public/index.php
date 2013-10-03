7954230c207eaa47db0bc1f0435394eabd62335f
Tim Lytle <tim@timlytle.net>
2013-10-03 12:19:20 -0700

    Updated from to be dynamic.

--- public/index.php
+++ public/index.php
@@ -13,12 +13,12 @@ require_once __DIR__ . '/../bootstrap.php'; //credentials and such
 $mongo = new MongoClient(MONGO);
 $db = $mongo->feedback;
 $nexmo = new Nexmo(NEXMO_KEY, NEXMO_SECRET);
-$feedback = new Feedback($nexmo, NEXMO_FROM, $db);
 
 //request looks to be from Nexmo
 $request = array_merge($_GET, $_POST); //method configurable via Nexmo API / Dashboard
 if(isset($request['msisdn'], $request['text'])){
     try{
+        $feedback = new Feedback($nexmo, $request['to'], $db);
         $feedback->process($request['msisdn'], $request['text']);
     } catch (Exception $e) {
         error_log($e); //NOTE: if you want Nexmo to retry, just give a non-2XX response
