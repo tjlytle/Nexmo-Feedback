61fa5b36883a2ca60e67a0e35286a8d34f925a57
Tim Lytle <tim@timlytle.net>
2016-04-13 09:38:37 -0400

    Fix for HTTPS

--- src/Nexmo.php
+++ src/Nexmo.php
@@ -17,7 +17,7 @@ class Nexmo
 
     public function sendSMS($to, $from, $text)
     {
-        $uri = sprintf('http://rest.nexmo.com/sms/json?username=%1$s&password=%2$s&from=%3$s&to=%4$s&text=%5$s', $this->key, $this->secret, $from, $to, urlencode($text));
+        $uri = sprintf('https://rest.nexmo.com/sms/json?username=%1$s&password=%2$s&from=%3$s&to=%4$s&text=%5$s', $this->key, $this->secret, $from, $to, urlencode($text));
         $ch = curl_init($uri);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $result = curl_exec($ch);
